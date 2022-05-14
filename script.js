5a8b0c5c32af291142e6a7001d6146088c2eae19
Dee <slawotsky@gmail.com>
2022-05-14 14:01:40 -0400

    Fixed visibility bug due to Udemy course setting opacity and allowing clickability whole hidden

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const toggle = document.getElementById('toggle');
 const nav = document.getElementById('nav');
 
+nav.classList.toggle('active');
+
+
 toggle.addEventListener('click', () => {
     nav.classList.toggle('active')
 })
