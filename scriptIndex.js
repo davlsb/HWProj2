8f4254b97c9c5b38f173c723ebf1e33209f07308
Dee <slawotsky@gmail.com>
2022-05-14 16:36:39 -0400

    fixed navbar on homepage

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,3 +1,14 @@
+const toggle = document.getElementById('toggle');
+const nav = document.getElementById('nav');
+
+nav.classList.toggle('active');
+
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('active')
+})
+
+
+
 const textEl = document.getElementById('info')
 const speedEl = document.getElementById('speed')
 const text = "We are Computer Science students from Queens College, New York, learning Web Development from our Professor Matthew Fried. <br> We are detail oriented, love to spend time on fixing the little details and optimizing our projects, and are able to follow own tasks as well as others, while understanding the dependencies that others have in the projects.<br>"
