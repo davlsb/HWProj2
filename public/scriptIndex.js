ef2721589b729da8286d54eba7d3e9e45b05dd3b
nirvanapersaud <nirvanapersaud88@gmail.com>
2022-05-22 00:44:32 -0400

    update scripts

--- public/scriptIndex.js
+++ public/scriptIndex.js
@@ -8,7 +8,7 @@ toggle.addEventListener('click', () => {
 })
 
 
-//text on the home page
+//Text on the home page
 const textEl = document.getElementById('info')
 const speedEl = document.getElementById('speed')
 const text = "We are Computer Science students from Queens College, New York, learning Web Development from our Professor Matthew Fried. <br> We are detail oriented, love to spend time on fixing the little details and optimizing our projects, and are able to follow own tasks as well as others, while understanding the dependencies that others have in the projects.<br>"
